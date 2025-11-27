4e696b0b7eb9864913889ca0cb93d4c906070517
upenderyarra <upenderyarra@gmail.com>
2025-11-27 19:53:48 +0530

    swagger implementation , Schema updated

--- src/main/java/com/example/employee_management_api/controller/EmployeeController.java
+++ src/main/java/com/example/employee_management_api/controller/EmployeeController.java
@@ -3,6 +3,9 @@ package com.example.employee_management_api.controller;
 import com.example.employee_management_api.dto.EmployeeDTO;
 import com.example.employee_management_api.entity.Employee;
 import com.example.employee_management_api.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +22,12 @@ public class EmployeeController {
     private final EmployeeService service;
 
     @PostMapping
+
+    @Operation(summary = "Create Employee")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Employee created"),
+            @ApiResponse(responseCode = "400", description = "Validation Failed")
+    })
     public Employee create(@Valid @RequestBody EmployeeDTO dto) {
         return service.createEmployee(dto);
     }
--- src/main/java/com/example/employee_management_api/dto/EmployeeDTO.java
+++ src/main/java/com/example/employee_management_api/dto/EmployeeDTO.java
@@ -1,5 +1,6 @@
 package com.example.employee_management_api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Data;
 
@@ -7,17 +8,22 @@ import lombok.Data;
 public class EmployeeDTO {
 
     @NotBlank(message = "Name cannot be empty")
+    @Schema(description = "Employee name", example = "John Doe")
     private String name;
 
     @NotBlank
+    @Schema(description = "Employee department", example = "HR")
     private String department;
 
     @Email
+    @Schema(description = "Employee email", example = "test@gmail.com")
     private String email;
 
     @Positive
+    @Schema(description = "Employee email", example = "10000")
     private Double salary;
 
     @Pattern(regexp = "^[0-9]{10}$")
+    @Schema(description = "Employee phone", example = "7893444444")
     private String phone;
 }
